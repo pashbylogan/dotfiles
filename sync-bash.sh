34346212cc2686ab4f0f94be9ee9dd0ce4278c87
Logan Pashby <pashbylogan@gmail.com>
2021-03-23 15:08:27 -0600

    for moving to new system

--- sync-bash.sh
+++ sync-bash.sh
@@ -2,22 +2,25 @@
 WARNED=false
 for dir in */; do
     if [ -d "${dir}dotfiles" ]; then 
-        for f in `ls -A "${dir}dotfiles`; do
-        if [ -e ~/$f ] && ! $WARNED; then
+        for f in `ls -A "${dir}dotfiles"`; do
+            if [ -e ~/$f ] && ! $WARNED; then
                 echo Warning: Files will be overwritten if you continue.
                 while true; do
-                        echo -n "Continue? (y/n): "
-                        read user_in
-                        if [ "$user_in" = "y" ]; then
-                                WARNED=true
-                                break
-                        elif [ "$user_in" = "n" ]; then
-                                exit 0
-                        fi
+                    echo -n "Continue? (y/n): "
+                    read user_in
+                    if [ "$user_in" = "y" ]; then
+                        WARNED=true
+                        break
+                    elif [ "$user_in" = "n" ]; then
+                        exit 0
+                    fi
                 done
+            fi
+        if [ "${f:0:1}" == "." ]; then 
+            echo Copying "${dir}dotfiles"/$f into place...
+            ln -s "${dir}dotfiles"/$f ~
         fi
-        echo Copying "${dir}dotfiles"/$f into place...
-        ln -s "${dir}dotfiles"/$f ~
+        done
     fi
 done
 echo Done
