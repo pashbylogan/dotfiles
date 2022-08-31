eee9b0873a4f64a1e3bd7820bd64e3a66c1969be
pashbylogan <pashbylogan@gmail.com>
2022-08-31 21:30:41 +0000

    bringing up to date with sauron changes

--- startup.sh
+++ startup.sh
@@ -1,17 +1,18 @@
+#!/bin/bash
 echo "Welcome! Let's start setting up your system. It could take more than 10 minutes, be patient"
-PATH = "~/projects/dotfiles/"
+PATH="~/projects/dotfiles/"
 
-echo "What name do you want to use in GIT user.name?"
-echo "For example, mine will be \"Logan Pashby\""
-read git_config_user_name
-
-echo "What email do you want to use in GIT user.email?"
-echo "For example, mine will be \"pashbylogan@gmail.com\""
-read git_config_user_email
-
-echo "What is your github username?"
-echo "For example, mine will be \"pashbylogan\""
-read username
+# echo "What name do you want to use in GIT user.name?"
+# echo "For example, mine will be \"Logan Pashby\""
+# read git_config_user_name
+# 
+# echo "What email do you want to use in GIT user.email?"
+# echo "For example, mine will be \"pashbylogan@gmail.com\""
+# read git_config_user_email
+# 
+# echo "What is your github username?"
+# echo "For example, mine will be \"pashbylogan\""
+# read username
 
 cd ~ && sudo apt-get update
 
@@ -31,9 +32,9 @@ sudo apt-get update && sudo apt-get install git -y
 echo 'Installing python3-pip'
 sudo apt-get install python3-pip -y
 
-echo "Setting up your git global user name and email"
-git config --global user.name "$git_config_user_name"
-git config --global user.email $git_config_user_email
+# echo "Setting up your git global user name and email"
+# git config --global user.name "$git_config_user_name"
+# git config --global user.email $git_config_user_email
 
 echo 'Cloning your .gitconfig from gist'
 cp $PATH+.gitconfig ~
@@ -46,10 +47,10 @@ if [$bool == 'Y' || $bool == 'y'] ; then
   cat ~/.ssh/id_ed25519.pub | xclip -selection clipboard
 fi
 
-echo 'Installing ZSH'
-sudo apt-get install zsh -y
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-chsh -s $(which zsh)
+# echo 'Installing ZSH'
+# sudo apt-get install zsh -y
+# sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# chsh -s $(which zsh)
 
 echo 'Copying dotfiles .zshrc to home'
 cp $PATH+.zshrc ~
@@ -110,7 +111,7 @@ cp -r $PATH+.tmux ~
 cd ~
 ln -s -f .tmux/.tmux.conf
 cd -
-cp $PATH+.tmux.conf.local .
+cp $PATH+.tmux.conf.local ~
 
 echo 'Updating and Cleaning Unnecessary Packages'
 sudo -- sh -c 'apt-get update; apt-get upgrade -y; apt-get full-upgrade -y; apt-get autoremove -y; apt-get autoclean -y'
