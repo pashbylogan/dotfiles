94f40f952ac028c9f184b1a7d8486f273412f8b8
Logan Pashby <pashbylogan@gmail.com>
2023-10-27 14:49:35 -0600

    checking on startup script

--- startup.sh
+++ startup.sh
@@ -47,10 +47,9 @@ if [$bool == 'Y' || $bool == 'y'] ; then
   cat ~/.ssh/id_ed25519.pub | xclip -selection clipboard
 fi
 
-# echo 'Installing ZSH'
-# sudo apt-get install zsh -y
-# sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-# chsh -s $(which zsh)
+echo 'Installing ZSH'
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+chsh -s $(which zsh)
 
 echo 'Copying dotfiles .zshrc to home'
 cp $PATH+.zshrc ~
@@ -67,18 +66,6 @@ echo 'Installing NodeJS LTS'
 nvm --version
 nvm install --lts
 nvm current
-
-# echo 'Installing VSCode'
-# curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-# sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-# sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-# sudo apt-get install apt-transport-https -y
-# sudo apt-get update && sudo apt-get install code -y
-
-# echo 'Installing Code Settings Sync'
-# code --install-extension Shan.code-settings-sync
-# sudo apt-get install gnome-keyring -y
-# cls
 
 echo 'Installing Docker'
 sudo apt-get purge docker docker-engine docker.io
